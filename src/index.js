69aef946d1b24feb05b9d445dc3cfbd9b06292d8
Angelo <angelespejo13@gmail.com>
2025-08-06 14:57:57 +0000

    fix index dep

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
  * File for import and export all function to use as a library
  */
 
-export { displayMappingTable } from "./ouija-generator.js"
+export { displayMappingTable, generatePhraseCoordinates } from "./ouija-generator.js"
 export { ouijaBoardMapping } from './ouija-board-mapping.js'
 export { startOuijaSession } from './ouija-session.js'
